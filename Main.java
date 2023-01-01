5d0ed3f014167f13f91a59e6b4825a965c3164f7
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2023-01-01 16:30:01 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("^-?[0-9]+");
+    private static final Pattern OPERATIONS_PATTERN = Pattern.compile("[+-]");
+    private static final Pattern EXTRA_SPACES_PATTERN = Pattern.compile("\\s+");
 
     public static void main(String[] args) {
         boolean quit = false;
@@ -13,12 +19,8 @@ public class Main {
             } else if (input.equals("/help")) {
                 System.out.println("The program calculates the sum of numbers");
             } else {
-                String[] splitInput = input.split(" ");
-
-                if (inputIsValid(splitInput)) {
-                    int[] numbers = parseInput(splitInput);
-                    int sum = calculateSum(numbers);
-                    System.out.println(sum);
+                if (inputIsValid(input)) {
+                    processInput(input);
                 }
             }
         }
@@ -28,20 +30,19 @@ public class Main {
 
     private static String getUserInput() {
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
+        return removeExtraSpaces(scanner.nextLine());
     }
 
-    private static boolean inputIsValid(String[] splitInput) {
-        boolean isValid = true;
+    private static String removeExtraSpaces(String text) {
+        Matcher matcher = EXTRA_SPACES_PATTERN.matcher(text);
+        text = matcher.replaceAll(" ");
+        return text;
+    }
 
-        for (int i = 0; i < splitInput.length; i++) {
-            if (!splitInput[i].matches("[-0-9]+")) {
-                isValid = false;
-                break;
-            }
-        }
-
-        return isValid;
+    private static boolean inputIsValid(String input) {
+        Pattern pattern = Pattern.compile("[-+0-9\\s]+");
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
     }
 
     private static int[] parseInput(String[] splitInput) {
@@ -52,6 +53,64 @@ public class Main {
         }
 
         return parsedInput;
+    }
+
+    private static void processInput(String input) {
+        String[] splitInput = input.split("\\s");
+        Queue<Integer> numbers = new LinkedList<>();
+        Queue<String> operations = new LinkedList<>();
+
+        for (String entry : splitInput) {
+            Matcher numberMatcher = NUMBER_PATTERN.matcher(entry);
+            Matcher operationsMatcher = OPERATIONS_PATTERN.matcher(entry);
+
+            if (numberMatcher.matches()) {
+                numbers.offer(Integer.parseInt(entry));
+            } else if (operationsMatcher.matches()) {
+                operations.offer(entry);
+            } else {
+                String[] splitOperations = entry.split("");
+                int minusCount = 0;
+                int plusCount = 0;
+
+                for (String op : splitOperations) {
+                    if (op.equals("-")) {
+                        minusCount++;
+                    } else {
+                        plusCount++;
+                    }
+                }
+
+                String evaluatedOperation = "+";
+
+                if (minusCount > 0) {
+                    if (minusCount % 2 != 0) {
+                        evaluatedOperation = "-";
+                    }
+                }
+
+                operations.offer(evaluatedOperation);
+            }
+        }
+
+        int lhs = 0;
+
+        if (!numbers.isEmpty()) {
+            lhs = numbers.poll();
+        }
+
+        while (!numbers.isEmpty() && !operations.isEmpty()) {
+            int rhs = numbers.poll();
+            String operation = operations.poll();
+
+            if (operation.equals("+")) {
+                lhs += rhs;
+            } else {
+                lhs -= rhs;
+            }
+        }
+
+        System.out.println(lhs);
     }
 
     private static int calculateSum(int[] numbers) {
