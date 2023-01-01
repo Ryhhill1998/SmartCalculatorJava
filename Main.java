bc7124af1ab6e183ecf5ea490e23276ac9074659
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2023-01-01 11:33:24 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,44 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    
+
     public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            int number1 = scanner.nextInt();
-            int number2 = scanner.nextInt();
-            int sum = add(number1, number2);
-            System.out.println(sum);
-        } catch (InputMismatchException e) {
-            System.out.println(e.getMessage());
+        boolean quit = false;
+
+        while (!quit) {
+            String input = getUserInput();
+
+            if (input.equals("/exit")) {
+                quit = true;
+            } else {
+                try {
+                    int[] numbers = parseInput(input);
+                    if (numbers.length == 1) {
+                        System.out.println(numbers[0]);
+                    } else {
+                        System.out.println(numbers[0] + numbers[1]);
+                    }
+                } catch (NumberFormatException ignored) {
+                }
+            }
         }
+
+        System.out.println("Bye!");
     }
 
-    private static int add(int number1, int number2) {
-        return number1 + number2;
+    private static String getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    private static int[] parseInput(String input) {
+        String[] splitInput = input.split(" ");
+        int[] parsedInput = new int[splitInput.length];
+
+        for (int i = 0; i < splitInput.length; i++) {
+            parsedInput[i] = Integer.parseInt(splitInput[i]);
+        }
+
+        return parsedInput;
     }
 }
