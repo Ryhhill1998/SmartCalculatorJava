a4b5256d66e1a1a58c270c61f208a39415becf78
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2023-01-04 08:12:45 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,9 +4,14 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-    private static final Pattern NUMBER_PATTERN = Pattern.compile("^[+-]?[0-9]+");
+    private static final Pattern DIGIT_PATTERN = Pattern.compile("\\d+");
+    private static final Pattern LETTER_PATTERN = Pattern.compile("[a-zA-Z]+");
+    private static final Pattern VALUE_PATTERN = Pattern.compile("^[+-]?\\d+");
     private static final Pattern OPERATIONS_PATTERN = Pattern.compile("[+-]+");
     private static final Pattern EXTRA_SPACES_PATTERN = Pattern.compile("\\s+");
+    private static final Pattern VARIABLE_ASSIGNMENT_PATTERN = Pattern.compile(".+=.+");
+    private static final Pattern VARIABLE_NAME_PATTERN = Pattern.compile("[a-zA-Z]+");
+    private static final Map<String, Integer> variableMap = new HashMap<>();
 
     public static void main(String[] args) {
         boolean quit = false;
@@ -22,14 +27,60 @@ public class Main {
                 } else {
                     System.out.println("Unknown command");
                 }
+            } else if (isVariableAssignment(input)) {
+                createVariable(input);
             } else if (!input.isEmpty()) {
-                if (!processInput(input)) {
-                    System.out.println("Invalid expression");
-                }
+                processInput(input);
             }
         }
 
         System.out.println("Bye!");
+    }
+
+    private static boolean isVariableAssignment(String input) {
+        Matcher matcher = VARIABLE_ASSIGNMENT_PATTERN.matcher(input);
+        return matcher.matches();
+    }
+
+    private static boolean variableNameIsValid(String variableName) {
+        Matcher matcher = VARIABLE_NAME_PATTERN.matcher(variableName);
+        return matcher.matches();
+    }
+
+    private static void createVariable(String input) {
+        input = input.replaceAll("\\s", "");
+        String[] splitInput = input.split("=");
+        String variableName = splitInput[0];
+        String variableValue = splitInput[1];
+
+        if (!variableNameIsValid(variableName)) {
+            System.out.println("Invalid identifier");
+        } else if (isDigit(variableValue)) {
+            updateVariableMap(variableName, Integer.parseInt(variableValue));
+        } else if (isAlpha(variableValue)) {
+            Integer value = variableMap.get(variableValue);
+            if (value != null) {
+                updateVariableMap(variableName, value);
+            } else {
+                System.out.println("Invalid assignment");
+            }
+        } else {
+            System.out.println("Invalid assignment");
+        }
+    }
+
+    private static boolean isDigit(String input) {
+        Matcher matcher = DIGIT_PATTERN.matcher(input);
+        return matcher.matches();
+    }
+
+    private static boolean isAlpha(String input) {
+        Matcher matcher = LETTER_PATTERN.matcher(input);
+        return matcher.matches();
+    }
+
+    private static void updateVariableMap(String key, int value) {
+        variableMap.put(key, value);
     }
 
     private static String getUserInput() {
@@ -43,34 +94,41 @@ public class Main {
         return text;
     }
 
-    private static boolean processInput(String input) {
+    private static void processInput(String input) {
         String[] splitInput = input.split("\\s");
-        Queue<Integer> numbers = new LinkedList<>();
+        Queue<Integer> values = new LinkedList<>();
         Queue<String> operations = new LinkedList<>();
 
         for (int i = 0; i < splitInput.length; i++) {
             String entry = splitInput[i];
-            Matcher numberMatcher = NUMBER_PATTERN.matcher(entry);
+            Matcher valueMatcher = VALUE_PATTERN.matcher(entry);
             Matcher operationsMatcher = OPERATIONS_PATTERN.matcher(entry);
 
-            if ((i % 2 == 0 || i == splitInput.length - 1) && !numberMatcher.matches()
-                    || i % 2 != 0 && !operationsMatcher.matches()) {
-                return false;
-            }
-
-            if (numberMatcher.matches()) {
-                numbers.offer(Integer.parseInt(entry));
-            } else if (operationsMatcher.matches()) {
+            if (i % 2 == 0) {
+                if (valueMatcher.matches()) {
+                    values.offer(Integer.parseInt(entry));
+                } else {
+                    Integer value = variableMap.get(entry);
+                    if (value == null) {
+                        System.out.println("Unknown variable");
+                        return;
+                    } else {
+                        values.offer(value);
+                    }
+                }
+            } else if (i != splitInput.length - 1 && operationsMatcher.matches()) {
                 if (entry.length() > 1) {
                     operations.offer(evaluateOperation(entry));
                 } else {
                     operations.offer(entry);
                 }
+            } else {
+                System.out.println("Invalid expression");
+                return;
             }
         }
 
-        evaluateExpression(numbers, operations);
-        return true;
+        evaluateExpression(values, operations);
     }
 
     private static String evaluateOperation(String operation) {
